a4bcfb7e1ec514b19dec3ddeff2788f9ba47d87c
EnkhAmar <enkhamar1220@gmail.com>
2022-05-28 21:13:07 +0800

    initial commit

---
+++ database/migrations/2022_05_28_130123_create_citizen_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citizen', function (Blueprint $table) {
+            $table->id();
+            $table->string("register_number")->unique();
+            $table->string("last_name");
+            $table->string("first_name");
+            $table->string("gender");
+            $table->date("dob");
+            $table->foreignId("family")->constrained("family");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citizen');
+    }
+};
---
+++ database/migrations/2022_05_28_130134_create_relation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relation', function (Blueprint $table) {
+            $table->foreignId("citizen_id")->constrained("citizen");
+            $table->foreignId("related_citizen_id")->constrained("citizen");
+            $table->foreignId("relation_type")->constrained("relation_type");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relation');
+    }
+};
